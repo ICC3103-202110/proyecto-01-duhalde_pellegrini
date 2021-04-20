7a566da51c482cb36fa983b800708218b8351a11
Camila Pellegrini <cvpellegrini@miuandes.cl>
2021-04-20 10:41:27 -0400

    captain

--- cards.py
+++ cards.py
@@ -20,21 +20,37 @@ class Cards:
                 
         
     def assasin(self):
-        if Player.money < 3:
+        if players[game.actual_turn].money < 3:
             raise ValueError('You need 3 coins to use this card')
         else:
-            Player.money -=3 
-            n = int(input('select which player do you want to kill'))
-            return n        
+            players[game.actual_turn].money += 3
+            player.coup()        
         
     def captain(self):
-        n = int(input('select from which player you want to steal coins'))
-        if playern.money < 2:
-            player.money += playern.money  #playern es al jugador que le quita, arreglar después 
-        else :
-            playern.money -=2 #lo mismo que arriba
-            player.money +=2
-
+        if len(players) == 3:
+            print('1) steal from', players[0].name)
+            print('2) steal from', players[1].name)
+            print('3) steal from', players[2].name)
+            steal = int(input('select from which player you want to steal coins'))
+            if player[steal-1].money < 2:
+                players[game.actual_turn].money += player[steal-1].money
+            else :
+                players[game.actual_turn].money += 2
+            print(players[steal-1].name, 'coins:',players[steal-1].money)
+            break
+        elif len(players) == 4:
+            print('1) kill', players[0].name)
+            print('2) kill', players[1].name)
+            print('3) kill', players[2].name)
+            print('4) kill', players[3].name)
+            steal = int(input('select from which player you want to steal coins'))
+            if player[steal-1].money < 2:
+                players[game.actual_turn].money += player[steal-1].money
+            else :
+                players[game.actual_turn].money += 2
+            print(players[steal-1].name, 'coins:',players[steal-1].money)
+            break
+       
     def ambassador(self):
         player.recieve_cards()
         print('select which cards you want to discard')
