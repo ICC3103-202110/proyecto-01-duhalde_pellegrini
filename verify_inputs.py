0d68acc46ac4685348f711e86d1cbe5fc823e6b2
Camila Pellegrini <cvpellegrini@miuandes.cl>
2021-04-22 19:14:31 -0400

    added verifier

--- verify_inputs.py
+++ verify_inputs.py
@@ -1,13 +1,39 @@
-
+from game import Game
 class Verify:
 
     def __init__(self):
 
-        self.verify_inputs = 0
+        self.verify_plays = verify_plays()
+        self.verify_choice_player = verify_choice_player()
     
     #no use yet
-    def verify_inputs(self, v):
+ '''   def verify_inputs(self, v):
         value = v + 1
         verify_options = list(range(1, value))
         print(verify_options)
-        return verify_options
+        return verify_options'''
+    def verify_plays(self,value):
+        if value > 8:
+            print('The choice has to be between 1 and 8')
+            return False
+        if value < 1:
+            print('The choice has to be between 1 and 8')
+            return False
+        else:
+            return True 
+    
+    def verify_choice_player(self,value):
+        if len(players) == 3:
+            if value > 3 or value < 1:
+                print('The choice has to be between 1 and 3')
+                return False
+            else:
+                return True
+        if len(players) == 4:
+            if value > 4 or value < 1:
+                print('The choice has to be between 1 and 4')
+                return False
+            else:
+                return True
+            
+            
